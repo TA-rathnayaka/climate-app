c11351523e93162193c72f02f77facfa40a8e9ec
Tharanga Anuradha <tharanga123anuradha@gmail.com>
2024-08-15 16:07:45 +0530

    add .env file to the folder and setup openwheather maps api

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+String? apiAddress;
+String? apiKey;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,10 +13,34 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double? latitude;
+  double? longitude;
+
   void getLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
-    print('latitude ${location.latitude} longitude ${location.longitude}');
+    latitude = location.latitude;
+    longitude = location.longitude;
+  }
+
+  void getData() async {
+    await dotenv.load(fileName: ".env");
+    apiAddress = dotenv.env['API_URL'] ?? '';
+    apiKey = dotenv.env['API_KEY'] ?? '';
+    var url = Uri.parse(
+        '${apiAddress}?lat=${latitude}&lon=${longitude}&appid=${apiKey}');
+    http.Response response = await http.get(url);
+    if (response.statusCode == 200) {
+      var data = JsonDecoder().convert(response.body);
+      double temperature = data['main']['temp'];
+      String cityName = data['name'];
+      int condition = data['weather'][0]['id'];
+      print(temperature);
+      print(cityName);
+      print(condition);
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
@@ -21,6 +51,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
   }
 }
